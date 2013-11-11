5d021e6284ff41ab86af0a6c008ad58064fffcaf
Yudha H Tejaningrat <yudha_ht@yahoo.com>
2013-11-11 18:59:00 +0700

    Fix footer

--- wp-themes/berita-panas-theme/footer.php
+++ wp-themes/berita-panas-theme/footer.php
@@ -1,14 +1,13 @@
 	</div><!-- #main -->
-	
+
 	<?php arras_before_footer() ?>
-    
+
     <div id="footer">
 		<div class="footer-sidebar-container clearfix">
-			<?php 
+			<?php
 				$footer_sidebars = arras_get_option('footer_sidebars');
 				if ($footer_sidebars == '') $footer_sidebars = 1;
-				
-				for ($i = 1; $i < $footer_sidebars + 1; $i++) : 
+				for ($i = 1; $i < $footer_sidebars + 1; $i++) :
 			?>
 				<ul id="footer-sidebar-<?php echo $i ?>" class="footer-sidebar clearfix xoxo">
 					<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Footer Sidebar #' . $i) ) : ?>
@@ -17,17 +16,17 @@
 				</ul>
 			<?php endfor; ?>
 		</div>
-		
+
 		<div class="footer-message">
 		<p class="floatright"><a class="arras" href="http://www.arrastheme.com/"><strong><?php _e('About Arras WordPress Theme', 'arras') ?></strong></a></p>
-		<?php echo stripslashes(arras_get_option('footer_message')); ?>		
+		<?php echo stripslashes(arras_get_option('footer_message')); ?>
 		</div><!-- .footer-message -->
     </div>
 </div><!-- #wrapper -->
-<?php 
+<?php
 arras_footer();
-wp_footer(); 
+wp_footer();
 ?>
 </body>
 </html>
-   
+
